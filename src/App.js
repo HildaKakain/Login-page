24a0a5c0ce1fa02afc60af3b42196d818b9aed14
HildaKakain <95189398+HildaKakain@users.noreply.github.com>
2023-07-10 23:18:05 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+// import Login from "./components/Login";
+import Login2 from "./components/Login2";
+
+function App() {
+  return (
+    <div className="App">
+        <Login2/>
+    </div>
+  );
+}
+
+export default App;
